afe5feb0cd8caa6370d74374bfcf5fb252f53b48
Fasalpa <psfr97@gmail.com>
2026-09-01 20:18:28 -0500

    feat: ccambio de estados en el mood

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ const bntSave = document.getElementById("btn-save-note");
 const inputTitle = document.getElementById("input-title");
 const textArea = document.getElementById("textarea-description");
 const colors = document.querySelectorAll(".btn-color");
+const moodSlider = document.getElementById("mood");
+const moodValue = document.getElementById("mood-value");
 
 const notes = [];
 let background = "";
@@ -31,4 +33,22 @@ function colorBtnNote() {
   });
 }
 
+moodSlider.addEventListener("input", () => {
+  const valueMood = moodSlider.value;
 
+  if (valueMood < 14.28) {
+    moodValue.textContent = "Muy mal 😫";
+  } else if (valueMood < 28.56) {
+    moodValue.textContent = "Mal 😔";
+  } else if (valueMood < 42.82) {
+    moodValue.textContent = "Ligeramente mal 😕";
+  } else if (valueMood < 57.12) {
+    moodValue.textContent = "Neutral 😐";
+  } else if (valueMood < 71.40) {
+    moodValue.textContent = "Ligeramente bien 😕";
+  } else if (valueMood < 42.82) {
+    moodValue.textContent = "Bien 😕";
+  } else {
+    moodValue.textContent = "Muy bien 😕";
+  }
+});
