1eef161e505376cc8e02e56931ce22b5a2cfdb97
Fasalpa <psfr97@gmail.com>
2026-09-02 23:23:52 -0500

    feat: agregar estado de fecha destruction y corrección de stiles.css
    stiles.css se duplicó parte del código

--- js/app.js
+++ js/app.js
@@ -4,26 +4,14 @@ const textArea = document.getElementById("textarea-description");
 const colors = document.querySelectorAll(".btn-color");
 const moodSlider = document.getElementById("mood");
 const moodValue = document.getElementById("mood-value");
+const destruction = document.getElementById("destruction");
+const timeDestruction = document.getElementById("time-self-destruction");
 
 const notes = [];
 let background = "";
+let stateMind = "";
+let valueTimeDestruction = "";
 colorBtnNote();
-
-bntSave.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  const newNote = {
-    title: inputTitle.value,
-    text: textArea.value,
-    colorBackground: background,
-  };
-
-  notes.push(newNote);
-  localStorage.setItem("notes", JSON.stringify(notes));
-  const notaGuardada = JSON.parse(localStorage.getItem("notes"));
-
-  console.log(notaGuardada);
-});
 
 function colorBtnNote() {
   colors.forEach((c) => {
@@ -34,21 +22,52 @@ function colorBtnNote() {
 }
 
 moodSlider.addEventListener("input", () => {
+
   const valueMood = moodSlider.value;
 
   if (valueMood < 14.28) {
     moodValue.textContent = "Muy mal 😫";
+    stateMind = moodValue.textContent;
   } else if (valueMood < 28.56) {
     moodValue.textContent = "Mal 😔";
+    stateMind = moodValue.textContent;
   } else if (valueMood < 42.82) {
     moodValue.textContent = "Ligeramente mal 😕";
+    stateMind = moodValue.textContent;
   } else if (valueMood < 57.12) {
     moodValue.textContent = "Neutral 😐";
-  } else if (valueMood < 71.40) {
-    moodValue.textContent = "Ligeramente bien 😕";
+    stateMind = moodValue.textContent;
+  } else if (valueMood < 71.4) {
+    moodValue.textContent = "Ligeramente bien 🙂";
   } else if (valueMood < 42.82) {
-    moodValue.textContent = "Bien 😕";
+    moodValue.textContent = "Bien 😊";
   } else {
-    moodValue.textContent = "Muy bien 😕";
+    moodValue.textContent = "Muy bien 😁";
   }
+});
+
+destruction.addEventListener("change", () => {
+  if (destruction.checked == true) {
+    timeDestruction.disabled = false;
+  } else {
+    timeDestruction.disabled = true;
+  }
+});
+
+
+
+bntSave.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const newNote = {
+    title: inputTitle.value,
+    text: textArea.value,
+    colorBackground: background,
+    mood: stateMind,
+    destruction: timeDestruction.value,
+  };
+  notes.push(newNote);
+  localStorage.setItem("notes", JSON.stringify(notes));
+  const notaGuardada = JSON.parse(localStorage.getItem("notes"));
+  console.log(notaGuardada);
 });
