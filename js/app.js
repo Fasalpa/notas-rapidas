79a2895361b1abd885b90b16564f0b740f11c82f
Fasalpa <psfr97@gmail.com>
2026-09-04 20:57:25 -0500

    feat: agregar autodestrucción, capsula y eliminación de localStorage

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ const capsule = document.getElementById("check-capsule");
 const timeCapsule = document.getElementById("unlock-date");
 const containerNotes = document.querySelector(".container-notes");
 
-const notes = JSON.parse(localStorage.getItem("notes")) || [];
+let notes = JSON.parse(localStorage.getItem("notes")) || [];
 let background = "#d0f0de";
 let stateMind = "Neutral 😐";
 let valueTimeDestruction = "";
@@ -25,7 +25,7 @@ function colorBtnNote() {
   colors.forEach((c) => {
     c.addEventListener("click", () => {
       if (background === "") {
-        background = getComputedStyle("background:red;");
+        background = "var(--btn-lemon)";
       } else {
         background = getComputedStyle(c).backgroundColor;
       }
@@ -104,13 +104,39 @@ function renderNotes() {
   } else {
     containerNotes.innerHTML = notes
       .map((nota) => {
+        // cápsula de tiempo
+        const validationDateCapsule = Boolean(nota.capsule);
+        const unlockTime = new Date(nota.capsule).getTime();
+        const now = Date.now();
+        const isLocked = validationDateCapsule && unlockTime > now;
+
+        const textToShow = isLocked
+          ? `🔒 <b>Esta nota es para tu yo futuro.</b>`
+          : nota.text;
+
+        const capsuleStatusHTML = validationDateCapsule
+          ? isLocked
+            ? `<p class="capsule-badge locked">🔒 Se desbloquea: ${new Date(nota.capsule).toLocaleString()}</p>`
+            : `<span class="capsule-badge unlocked">🔓 Cápsula Abierta</span>`
+          : "";
+
+        // autodestrucción
+
+        const hasDestruction = Boolean(nota.destruction);
+
+        const timeToShow = hasDestruction
+          ? `<p class="destruction-badge"><b>Destrucción: <br> ${timeRemaining(nota.destruction)}</b></p>`
+          : "";
+
         return `
           <article class="card-nota" style="background-color: ${nota.colorBackground || "var(--btn-lemon)"};">
             <h3 class="card-nota-title">${nota.title}</h3>
-            <p class="card-nota-text">${nota.text}</p>
+            <p class="card-nota-text">${textToShow}</p>
             
             <div class="card-nota-footer">
-              <p class="card-nota-date">Fecha: ${new Date(nota.date).toLocaleString()}</p>
+              <p class="card-nota-date">Creado: ${new Date(nota.date).toLocaleString()}</p>
+              ${capsuleStatusHTML}
+              ${timeToShow}
             </div>
             
             <div class="card-nota-meta">
@@ -123,30 +149,69 @@ function renderNotes() {
   }
 }
 
-form.addEventListener("submit", () => {
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const capsuleDate = capsule.checked ? timeCapsule.value : null;
+  const destructionDate =
+    destruction.checked && timeDestruction.value
+      ? new Date(timeDestruction.value).getTime()
+      : null;
+
   const newNote = {
     id: crypto.randomUUID(),
     title: inputTitle.value,
     text: textArea.value,
     colorBackground: background,
     mood: stateMind,
-    destruction: timeDestruction.value,
-    capsule: timeCapsule.value,
+    destruction: destructionDate,
+    capsule: capsuleDate,
     date: Date.now(),
   };
   notes.push(newNote);
   localStorage.setItem("notes", JSON.stringify(notes));
+
   renderNotes();
   resetValues();
 });
 
+function timeRemaining(timeStamp) {
+  const difference = timeStamp - Date.now();
+
+  if (difference <= 0) {
+    return "🌋 Nota destruida";
+  }
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((difference / (1000 * 60)) % 60);
+  const seconds = Math.floor((difference / 1000) % 60);
+
+  return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+}
+
+function destroyExpiredNotes() {
+  const now = Date.now();
+
+  notes = notes.filter((nota) => {
+    return !nota.destruction || nota.destruction > now;
+  });
+  localStorage.setItem("notes", JSON.stringify(notes));
+}
+
 function resetValues() {
-  inputTitle.value = "";
+inputTitle.value = "";
   textArea.value = "";
   moodSlider.value = 50;
   moodValue.textContent = "Neutral 😐";
   destruction.checked = false;
+  timeDestruction.value = "";
   timeDestruction.disabled = true;
   capsule.checked = false;
+  timeCapsule.value = "";
   timeCapsule.disabled = true;
+  btnSave.disabled = true;
 }
+// refrescar para la cuenta regresiva
+setInterval(() => {
+  destroyExpiredNotes();
+  renderNotes();
+}, 1000);
